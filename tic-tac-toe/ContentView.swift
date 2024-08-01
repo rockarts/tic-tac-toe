c2acf4fa985e83c51519660e1f1d9813177ddd46
Steven Rockarts <steven@airtrail.co>
2024-08-01 13:34:48 -0600

    Initial Commit

---
+++ tic-tac-toe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tic-tac-toe
+//
+//  Created by Steven Rockarts on 2024-08-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tic-tac-toe/tic_tac_toeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tic_tac_toeApp.swift
+//  tic-tac-toe
+//
+//  Created by Steven Rockarts on 2024-08-01.
+//
+
+import SwiftUI
+
+@main
+struct tic_tac_toeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
